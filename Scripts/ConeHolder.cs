083c1aef517e757437ba4aa976da649f027480cd
3xTomorrow <denyszagrebin7@gmail.com>
2025-01-26 21:55:00 -0500

    Added a feature that prevents the player from forever spawning cones in their hand.

--- Scripts/ConeHolder.cs
+++ Scripts/ConeHolder.cs
@@ -1,12 +1,19 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConeHolder : MonoBehaviour
 {
     [SerializeField] private Transform conePrefab;
+    [SerializeField] private bool hasCone = false;
 
     public void SpawnCone()
     {
         Transform cone = Instantiate(conePrefab, transform.position, transform.rotation);
         cone.SetParent(transform);
+        if (hasCone)
+        {
+            Destroy(cone.gameObject);
+        }
+        hasCone = true;
     }
 }
